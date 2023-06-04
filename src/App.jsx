39c536834d06bb21d0f0069a03d3e2bfc311612d
Ondra Svec <ondrasvecveslo@gmail.com>
2023-06-04 19:38:07 +0200

    Fetch characters data and set state for displaying cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [characters, setCharacters] = useState([]);
+
+  const fetchCharacters = async () => {
+    const response = await fetch(
+      "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json"
+    );
+    const data = await response.json();
+    return data;
+  };
+
+  const getTenRandomChar = (arr) => {
+    const tenRandomChars = new Set();
+    while (tenRandomChars.size < 10) {
+      const randomInd = Math.floor(Math.random() * arr.length);
+      tenRandomChars.add(arr[randomInd]);
+    }
+    setCharacters([...tenRandomChars]);
+  };
+
+  useEffect(() => {
+    fetchCharacters().then((data) => getTenRandomChar(data));
+  }, []);
+
+  const charElements = characters.map((char) => (
+    <img key={char.id} src={char.images.md} />
+  ));
+  return <>{characters && charElements}</>;
 }
 
 export default App;
