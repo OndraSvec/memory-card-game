49ef092a4277c557093e67d9f6057238baa05136
Ondra Svec <ondrasvecveslo@gmail.com>
2023-06-05 12:04:14 +0200

    Add onMouseMove card glow effect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,27 @@
-const Card = ({ children, onClick, cardClicked }) => (
-  <div className={cardClicked ? "card active" : "card"} onClick={onClick}>
-    <div className="card-inner">
-      <div className="card-front">{children}</div>
-      <div className="card-back">
-        <p>memory card game</p>
+const Card = ({ children, onClick, cardClicked }) => {
+  const handleMouseMove = (e) => {
+    const { target } = e;
+    const rect = target.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+  };
+  return (
+    <div
+      className={cardClicked ? "card active" : "card"}
+      onClick={onClick}
+      onMouseMove={handleMouseMove}
+    >
+      <div className="card-inner">
+        <div className="card-front">{children}</div>
+        <div className="card-back">
+          <p>memory card game</p>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Card;
